eaf5618f72e247ffb088ce67606acb295b1d174a
Steven Medina <smedina2494@gmail.com>
2023-04-04 19:39:25 -0400

    WIP: Fixes links to work with future URL changes

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('about/', views.about),
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
     path('cards/', views.cards_index, name='cards_index'),
     path('cards/<int:card_id>/', views.cards_detail, name='cards_detail')
 ]
